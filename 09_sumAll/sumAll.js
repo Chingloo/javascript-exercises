ac0bd349dd38eefd1b42fb82eb7cb55e8ed468a9
Muskan Thakur <cherry@localhost.localdomain>
2025-07-06 14:38:41 +0530

    Sum all done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(max, min) {
+    if(!Number.isInteger(max)|| !Number.isInteger(min))
+        return "ERROR!"
+    if(min<0 || max <0)
+         return "ERROR"
+    if min > max {
+        const temp =min;
+        min=max;
+        max=temp;
+    }
+let sum =0;
+for(int i =min;i<=max;i++){
+    sum += i;
+}
+return sum 
 };
 
 // Do not edit below this line
